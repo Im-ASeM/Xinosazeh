9e9a8f8a6f5c2afdf0c03b5038ee858f3dba68c5
Abolfazl ASeM <a.semnani12@gmail.com>
2024-12-07 15:58:03 +0330

    fix linkNav

--- Controllers/WorksController.cs
+++ Controllers/WorksController.cs
@@ -11,22 +11,29 @@ public class WorksController : Controller
     public IActionResult Details(int Id)
     {
         var post = db.WorkPosts_tbl.Find(Id);
-        string title = post.Title;
-        string discription =post.Discription;
-        string body =post.body;
-        string footer = post.footer;
-        List<string> images = post.images;
-       
-        string mainImg = post.mainImg;
+        if (post != null)
+        {
+            string title = post.Title;
+            string discription = post.Discription;
+            string body = post.body;
+            string footer = post.footer;
+            List<string> images = post.images;
+            string mainImg = post.mainImg;
+
+            ViewBag.title = title;
+            ViewBag.discription = discription;
+            ViewBag.body = body;
+            ViewBag.images = images;
+            ViewBag.mainImg = mainImg;
+            ViewBag.footer = footer;
+
+            return View();
+        }
+        else
+        {
+            return RedirectToAction("index", "home");
+        }
 
 
-        ViewBag.title = title;
-        ViewBag.discription = discription;
-        ViewBag.body = body;
-        ViewBag.images = images;
-        ViewBag.mainImg = mainImg;
-        ViewBag.footer = footer;
-
-        return View();
     }
 }
